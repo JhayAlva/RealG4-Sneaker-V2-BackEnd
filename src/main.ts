1cf9356b4177e76c9a748214fd4f1909bbd91643
Jeremie <jeremiechiclote34@gmail.com>
2026-05-30 14:09:45 +0200

    Cambios main

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'https://realgsneaker.netlify.app',
+    origin: 'https://realg4sneaker.netlify.app',
     methods: 'GET,POST,PUT,DELETE,PATCH',
     credentials: true
   });
