ccaee506770cfae5b7aae4a101c8e8bd26d4342c
Jeremie <jeremiechiclote34@gmail.com>
2026-05-30 13:59:08 +0200

    Cambios para vercel

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
-
 import { AppModule } from './app.module';
 import * as express from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors(),
+
+  app.enableCors({
+    origin: 'https://realgsneaker.netlify.app',
+    methods: 'GET,POST,PUT,DELETE,PATCH',
+    credentials: true
+  });
+
   app.use(express.json({ limit: '50mb' }));
   app.use(express.urlencoded({ limit: '50mb', extended: true }));
-  
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
@@ -17,7 +22,6 @@ async function bootstrap() {
     })
   );
 
-
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
